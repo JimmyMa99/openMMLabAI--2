e8dc0784381374dd63d8eb7bcde78965e470f602
Ma Zhiming <101508488+JimmyMa99@users.noreply.github.com>
2023-02-09 00:43:36 +0800

    修复：自定义视频时出现未被识别对象报错
    自定义视频时出现未被识别对象将跳过操作
    加入了进度条和提醒

--- 基础作业/new_script/color_splash.py
+++ 基础作业/new_script/color_splash.py
@@ -2,14 +2,15 @@ import cv2
 import mmcv
 import numpy as np
 import torch
+from tqdm import tqdm
 
 from mmdet.apis import init_detector, inference_detector
 
 score_thr = 0.8
-video_reader = mmcv.VideoReader("dataset/test_video.mp4")
+video_reader = mmcv.VideoReader("dataset/ec89756f0ba7dcc023f55d24611a40d3.mp4")
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video_writer = cv2.VideoWriter(
-    './color_splash.mp4', fourcc, video_reader.fps,
+    './my_color_splash.mp4', fourcc, video_reader.fps,
     (video_reader.width, video_reader.height))
 print(len(video_reader))
 
@@ -17,7 +18,7 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 model = init_detector('new_mrcnn/mask_rcnn_r50_fpn_1x_ballon.py', 'new_mrcnn/latest.pth', device=device)
 
-for frame in video_reader:
+for frame in tqdm(video_reader):
     result = inference_detector(model, frame)
     mask = None
     masks = result[1][0]
@@ -29,28 +30,30 @@ for frame in video_reader:
                 mask = masks[i]
 
     # 获取各通道mask像素
+    try:
+        masked_b = frame[:, :, 0] * mask
+        masked_g = frame[:, :, 1] * mask
+        masked_r = frame[:, :, 2] * mask
+        masked = np.concatenate([masked_b[:,:,None],masked_g[:,:,None],masked_r[:,:,None]],axis=2)
 
-    masked_b = frame[:, :, 0] * mask
-    masked_g = frame[:, :, 1] * mask
-    masked_r = frame[:, :, 2] * mask
-    masked = np.concatenate([masked_b[:,:,None],masked_g[:,:,None],masked_r[:,:,None]],axis=2)
+        # frame转灰度图
 
-    # frame转灰度图
+        un_mask = 1 - mask
+        frame_b = frame[:, :, 0] * un_mask
+        frame_g = frame[:, :, 1] * un_mask
+        frame_r = frame[:, :, 2] * un_mask
+        frame = np.concatenate([frame_b[:, :, None], frame_g[:, :, None], frame_r[:, :, None]], axis=2).astype(np.uint8)
+        frame = mmcv.bgr2gray(frame, keepdim=True)
+        frame = np.concatenate([frame, frame, frame], axis=2)
+        # mask加到灰度图中
 
-    un_mask = 1 - mask
-    frame_b = frame[:, :, 0] * un_mask
-    frame_g = frame[:, :, 1] * un_mask
-    frame_r = frame[:, :, 2] * un_mask
-    frame = np.concatenate([frame_b[:, :, None], frame_g[:, :, None], frame_r[:, :, None]], axis=2).astype(np.uint8)
-    frame = mmcv.bgr2gray(frame, keepdim=True)
-    frame = np.concatenate([frame, frame, frame], axis=2)
-    # mask加到灰度图中
-
-    frame += masked
-
+        frame += masked
+    except:
+        print('Did not find the balloon!')
+        pass
     # frame = model.show_result(frame, result, score_thr=score_thr)
     video_writer.write(frame)
-
+print('Done!')
 #   掩码计算->灰度图->灰度图*3->替换值
 
 video_writer.release()
